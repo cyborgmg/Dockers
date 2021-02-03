460a894358400931541ce572f65ae6d67c9a6a38
cyborgmg <cyborgmg@gmail.com>
2021-02-03 09:16:13 -0300

    update

--- postgres/run.sh
+++ postgres/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name postgres --hostname postgres -v /home/cyborg/Dockers/postgres/data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=c6b94gmg -e POSTGRES_USER=postgres -e POSTGRES_DB=public -p 5432:5432 postgres:latest
+docker run -d --name postgres --hostname postgres -v /home/cyborg/Dockers/postgres/data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=c6b94gmg -e POSTGRES_USER=postgres -e POSTGRES_DB=postgres -p 5432:5432 postgres:latest
--- sonar/run-postgres.sh
+++ sonar/run-postgres.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name pg-sonar --hostname pg-sonar -v /home/cyborg/Dockers/sonar/postgresql:/var/lib/postgresql -e POSTGRES_PASSWORD=sonar -e POSTGRES_USER=sonar -e POSTGRES_DB=sonar -p 5432:5432 postgres:9.6
+docker run -d --name pg-sonar --hostname pg-sonar -v /home/cyborg/Dockers/sonar/postgresql/data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=sonar -e POSTGRES_USER=sonar -e POSTGRES_DB=sonar -p 5432:5432 postgres:latest
